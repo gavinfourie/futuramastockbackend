50e60f4f90e9b2b43cb362b6c07065b1850b2a2b
Gavin Fourie <gavinfourie07@gmail.com>
2020-11-21 13:54:34 +0200

    Config cors

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ mongoose.connection.on("connected", (err, res) => {
 let corsOptions = {
   origin: ['https://futuramastock.herokuapp.com', 'http://futuramastock.herokuapp.com'],
   methods: ['GET', 'DELETE', 'POST', 'HEAD', 'OPTIONS', 'PUT'],
-  allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Origin']
+  allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Origin'],
+  credentials: true
 }
 
 app.use(cors(corsOptions))
