5b65963b92799597910468dea7083ccff85a6e14
Gavin Fourie <gavinfourie07@gmail.com>
2020-11-22 17:35:25 +0200

    Config cors

--- routes/upload.js
+++ routes/upload.js
@@ -9,7 +9,7 @@ let items = []
 let finalItems = []
 let failedItems = []
 
-router.post('/excel', async (req, res) => {
+router.post('/excel', async (req, res, next) => {
   items = []
   const form = new formidable.IncomingForm()
   try {
