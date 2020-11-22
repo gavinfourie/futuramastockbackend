6f0455c59be9f360aefdd83e596975cc92748942
Gavin Fourie <gavinfourie07@gmail.com>
2020-11-22 17:23:04 +0200

    Config cors

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const port = process.env.PORT || 3030
 const product = require('./models/stock')
 const mongoose = require('mongoose')
 const uri = process.env.DATABASE_URL
+
+let corsOptions = {
+  origin: ['https://futuramastock.herokuapp.com', 'http://futuramastock.herokuapp.com'],
+  methods: ['GET', 'DELETE', 'POST', 'HEAD', 'OPTIONS', 'PUT'],
+  allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Origin', 'X-Requested-With'],
+  credentials: true
+}
+
+app.options('*', cors(corsOptions))
+
+app.use(cors(corsOptions))
+
 let upload = require('./routes/upload')
 let stockcheck = require('./routes/stockcheck')
 
@@ -18,16 +30,6 @@ mongoose.connection.on("connected", (err, res) => {
   console.log("mongoose is connected")
 })
 
-let corsOptions = {
-  origin: ['https://futuramastock.herokuapp.com', 'http://futuramastock.herokuapp.com'],
-  methods: ['GET', 'DELETE', 'POST', 'HEAD', 'OPTIONS', 'PUT'],
-  allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Origin', 'X-Requested-With'],
-  credentials: true
-}
-
-app.options('*', cors(corsOptions))
-
-app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/upload', upload)
 app.use('/stockcheck', stockcheck)
